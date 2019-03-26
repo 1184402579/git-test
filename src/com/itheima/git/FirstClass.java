220fccc23850645f6547b386c6d4f516153fb474
zhangsan <zhangsan@itcast.cn>
2019-03-26 20:05:21 +0800

    把本地工程添加到git

---
+++ src/com/itheima/git/FirstClass.java
@@ -0,0 +1,17 @@
+package com.itheima.git;
+
+/**
+ * @author 郭港
+ * @description
+ * @date 2019/3/26 19:56
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
